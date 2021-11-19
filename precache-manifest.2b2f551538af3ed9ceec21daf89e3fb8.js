5a1e6ce13c3ec7c93b78382eb275940f9507e4d1
jheggen <heggen.josh@gmail.com>
2021-11-19 11:59:48 -0600

    Updates

---
+++ precache-manifest.2b2f551538af3ed9ceec21daf89e3fb8.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649cade3cbe062005d199c3ed84971d0",
+    "url": "/dragonSlayer/index.html"
+  },
+  {
+    "revision": "e4d04a28944542ff435f",
+    "url": "/dragonSlayer/static/css/2.90a281a6.chunk.css"
+  },
+  {
+    "revision": "5163dd21b23c8a901620",
+    "url": "/dragonSlayer/static/css/main.25bdbdbe.chunk.css"
+  },
+  {
+    "revision": "e4d04a28944542ff435f",
+    "url": "/dragonSlayer/static/js/2.7d2f80f0.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/dragonSlayer/static/js/2.7d2f80f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5163dd21b23c8a901620",
+    "url": "/dragonSlayer/static/js/main.42181577.chunk.js"
+  },
+  {
+    "revision": "821c9a5e5a957aeebdcc",
+    "url": "/dragonSlayer/static/js/runtime-main.3c1dbf0a.js"
+  },
+  {
+    "revision": "ea39ee31025f0be39741796f521b91b8",
+    "url": "/dragonSlayer/static/media/armor.ea39ee31.png"
+  },
+  {
+    "revision": "b043a7634b9957f4426bea38e7cb875d",
+    "url": "/dragonSlayer/static/media/bam.b043a763.jpg"
+  },
+  {
+    "revision": "0d1f89a9c2244e61463ca89138339f09",
+    "url": "/dragonSlayer/static/media/bomb.0d1f89a9.png"
+  },
+  {
+    "revision": "9d32489595048f814bd5e68f7cc0a8ff",
+    "url": "/dragonSlayer/static/media/dragon.9d324895.jpg"
+  },
+  {
+    "revision": "e9e94c6aeccbc7528656a7771e94c636",
+    "url": "/dragonSlayer/static/media/flamethrower.e9e94c6a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/dragonSlayer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/dragonSlayer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/dragonSlayer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/dragonSlayer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/dragonSlayer/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "80afcc437b252a7be8c9f58a4823526e",
+    "url": "/dragonSlayer/static/media/ghost.80afcc43.png"
+  },
+  {
+    "revision": "99fd83c8f9f9153e2027dde644f50f9b",
+    "url": "/dragonSlayer/static/media/gun.99fd83c8.png"
+  },
+  {
+    "revision": "e230de630f77d87f376acc1dc6075122",
+    "url": "/dragonSlayer/static/media/landmark.e230de63.jpg"
+  },
+  {
+    "revision": "4574b8119afb3e8862c4016fc3cf9c41",
+    "url": "/dragonSlayer/static/media/lightning.4574b811.jpg"
+  },
+  {
+    "revision": "76669a5b3ae07fb6f8c7776bfc1b8b8a",
+    "url": "/dragonSlayer/static/media/poison.76669a5b.png"
+  },
+  {
+    "revision": "7af3133562546f2db8461a159c2ea2b7",
+    "url": "/dragonSlayer/static/media/potion.7af31335.png"
+  },
+  {
+    "revision": "f141faac2dda2fd95405082d3cc6fabe",
+    "url": "/dragonSlayer/static/media/raygun.f141faac.jpg"
+  },
+  {
+    "revision": "105446ccab77f6ac30e44b31a90485de",
+    "url": "/dragonSlayer/static/media/robot.105446cc.png"
+  },
+  {
+    "revision": "f1db75e141b37aa16c2f9d9a76e89769",
+    "url": "/dragonSlayer/static/media/shark.f1db75e1.png"
+  },
+  {
+    "revision": "ff2b056eefbbbd2bc72e0289ccc85447",
+    "url": "/dragonSlayer/static/media/sword.ff2b056e.jpg"
+  },
+  {
+    "revision": "53ad4d969b552f8207253b5253747198",
+    "url": "/dragonSlayer/static/media/warrior.53ad4d96.png"
+  },
+  {
+    "revision": "24a759cb3e6c29b8ab00e76e6b6bfa2e",
+    "url": "/dragonSlayer/static/media/zombie.24a759cb.png"
+  }
+]);
